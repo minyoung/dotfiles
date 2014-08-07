d8ff15c22f0b024f235a188a24511d36e6b6661f
Min-Young Wu <mwu368@gmail.com>
2014-08-07 13:13:24 -0700

    Adding an abspath function (for when there's no realpath)

--- bin/multiple-git-checkouts.sh
+++ bin/multiple-git-checkouts.sh
@@ -14,6 +14,24 @@ EOHELP
   exit 1
 fi
 
+# for when you don't have realpath...
+function abspath() {
+    pushd . > /dev/null
+    if [ -d "$1" ]; then
+        cd "$1"
+        dirs -l +0
+    else
+        cd "`dirname \"$1\"`"
+        cur_dir=`dirs -l +0`
+        if [ "$cur_dir" == "/" ]; then
+            echo "$cur_dir`basename \"$1\"`"
+        else
+            echo "$cur_dir/`basename \"$1\"`"
+        fi
+    fi
+    popd > /dev/null;
+}
+
 source=$(realpath "$1")
 destination="$2"
 
