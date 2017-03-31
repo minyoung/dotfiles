b4e951baa596269ed51a3818586b3f4d21639ad5
Min-Young Wu <minyoung@carbon3d.com>
2017-03-30 18:36:45 -0700

    jump fixes
    * try to auto-expand mark names
    * quote mark path so that it can contain spaces

--- .sh/jump/jump.sh
+++ .sh/jump/jump.sh
@@ -5,19 +5,28 @@ function jump {
     marks
     return
   fi
-  cd -P $MARKPATH/$1 2>/dev/null || echo "No such mark: $1"
+  if [[ -L "$MARKPATH/$1" ]]; then
+    cd -P "$MARKPATH/$1" 2>/dev/null || echo "Error going to mark: $1"
+  else
+    links=("$MARKPATH/$1"*)
+    if [[ ${#links[@]} == 1 ]]; then
+      cd -P "${links[1]}" 2>/dev/null || echo "Error going to mark: ${links[1]}"
+    else
+      echo "No such mark: $1"
+    fi
+  fi
 }
 
 function mark {
-  mkdir -p $MARKPATH ; ln -s $(pwd) $MARKPATH/$1
+  mkdir -p "$MARKPATH" ; ln -s "$(pwd)" "$MARKPATH/$1"
 }
 
 function unmark {
-  rm -i $MARKPATH/$1
+  rm -i "$MARKPATH/$1"
 }
 
 function marks {
-  ls -l $MARKPATH | sed 's/  / /g' | cut -d' ' -f9- | column -t
+  ls -l "$MARKPATH" | sed 's/  / /g' | cut -d' ' -f9- | column -t
   # ls -l $MARKPATH | cut -d' ' -f9- | sed 's/ -/\t-/g' && echo
   # ls -l $MARKPATH | awk '/->/ {printf("%-20s -> %s\n", $9, $11)}'
 }
