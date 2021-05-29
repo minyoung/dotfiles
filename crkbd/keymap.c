e02becd613ae7589d6d3d29135a235709c9f3585
Min-Young Wu <mwu368@gmail.com>
2021-05-28 17:47:59 -0700

    home row mod tap keys

--- crkbd/keymap.c
+++ crkbd/keymap.c
@@ -21,7 +21,18 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // tap dance
 enum {
   D_COLN,
+  D_SCLN,
+  D_CIRC,
+  D_PERC,
+  D_DLR,
+  D_TILD,
   SOME_OTHER_DANCE,
+};
+
+
+enum custom_keycodes {
+  LT_LOWER = SAFE_RANGE,
+  LT_RAISE,
 };
 
 
@@ -37,21 +48,48 @@ enum layers {
 #define _LG_ESC LGUI_T(KC_ESC)
 #define _RG_ENT RGUI_T(KC_ENT)
 #define _L1_ESC LT(1, KC_ESC)
-#define _L1_SPC LT(1, KC_SPC)
+// #define _L1_SPC LT(1, KC_SPC)
+#define _L1_SPC LT_LOWER
 #define _L2_SPC LT(2, KC_SPC)
+// #define _L2_SPC LT_RAISE
 #define _RS_ESC RSFT_T(KC_ESC)
 #define _LC_GRV LCTL_T(KC_GRV)
 
-#define _MOD__J LCTL_T(KC_J)
-#define _MOD__K LALT_T(KC_K)
-#define _MOD__L LGUI_T(KC_L)
+// home row mod keys
+// base layer
+#define HM____J LCTL_T(KC_J)
+#define HM____K LALT_T(KC_K)
+#define HM____L LGUI_T(KC_L)
 // #define _MOD_CN LSFT_T(KC_SCLN)
-#define _MOD_CN TD(D_COLN)
+#define HM___SC TD(D_SCLN)
 
-#define _MOD__F RCTL_T(KC_F)
-#define _MOD__D RALT_T(KC_D)
-#define _MOD__S RGUI_T(KC_S)
-#define _MOD__A RSFT_T(KC_A)
+#define HM____F RCTL_T(KC_F)
+#define HM____D RALT_T(KC_D)
+#define HM____S RGUI_T(KC_S)
+#define HM____A RSFT_T(KC_A)
+
+// lower layer
+#define HM_DOWN LCTL_T(KC_DOWN)
+#define HM___UP LALT_T(KC_UP)
+#define HM_RGHT LGUI_T(KC_RGHT)
+// #define HM___R4 LSFT_T(KC_SCLN)
+#define HM___R4 KC_LSFT
+
+#define HM____6 RCTL_T(KC_6)
+#define HM____5 RALT_T(KC_5)
+#define HM____4 RGUI_T(KC_4)
+#define HM__GRV RSFT_T(KC_GRV)
+
+// raise layer
+#define HM_PGDN LCTL_T(KC_PGDN)
+#define HM_PGUP LALT_T(KC_PGUP)
+#define HM__END LGUI_T(KC_END)
+#define HM_COLN TD(D_COLN)
+
+#define HM_CIRC TD(D_CIRC)
+#define HM_PERC TD(D_PERC)
+#define HM__DLR TD(D_DLR)
+#define HM_TILD TD(D_TILD)
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -59,9 +97,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _LC_GRV, _MOD__A, _MOD__S, _MOD__D, _MOD__F,    KC_G,                         KC_H, _MOD__J, _MOD__K, _MOD__L, _MOD_CN, KC_QUOT,
+      _LC_GRV, HM____A, HM____S, HM____D, HM____F,    KC_G,                         KC_H, HM____J, HM____K, HM____L, HM___SC, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, _RS_ESC,
+      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_CAPS,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                             MO(2), _L1_SPC, _LG_ESC,    _RG_ENT, _L2_SPC,   MO(1)
                                       //`--------------------------'  `--------------------------'
@@ -72,7 +110,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_TAB, XXXXXXX, KC_7   , KC_8   , KC_9   , KC_0   ,                      KC_LBRC, KC_RBRC, KC_INS , XXXXXXX, KC_DEL , KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL, KC_GRV , KC_4   , KC_5   , KC_6   , KC_MINS,                      KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, XXXXXXX, KC_GRV ,
+      KC_LCTL, HM__GRV, HM____4, HM____5, HM____6, KC_MINS,                      KC_LEFT, HM_DOWN, HM___UP, HM_RGHT, HM___R4, KC_GRV ,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT, KC_0   , KC_1   , KC_2   , KC_3   , KC_EQL ,                      KC_BSLS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RSFT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
@@ -84,7 +122,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_TAB, XXXXXXX, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,                      KC_LCBR, KC_RCBR, XXXXXXX, XXXXXXX, KC_DEL , KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL, KC_TILD, KC_DLR , KC_PERC, KC_CIRC, KC_UNDS,                      KC_HOME, KC_PGDN, KC_PGUP, KC_END , KC_COLN, KC_DQUO,
+      KC_LCTL, HM_TILD, HM__DLR, HM_PERC, HM_CIRC, KC_UNDS,                      KC_HOME, HM_PGDN, HM_PGUP, HM__END, HM_COLN, KC_DQUO,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT, XXXXXXX, KC_EXLM, KC_AT  , KC_HASH, KC_PLUS,                      KC_PIPE, XXXXXXX, KC_LABK, KC_RABK, KC_QUES, KC_RSFT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
@@ -98,7 +136,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX, KC_CAPS,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LGUI, _______,  KC_SPC,     KC_ENT, _______, KC_RALT
                                       //`--------------------------'  `--------------------------'
@@ -210,13 +248,59 @@ void oled_task_user(void) {
     }
 }
 
+#endif // OLED_DRIVER_ENABLE
+
+bool lower_interrupted = false;
+bool raise_interrupted = false;
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+#ifdef OLED_DRIVER_ENABLE
   if (record->event.pressed) {
     set_keylog(keycode, record);
   }
+#endif
+
+  // unfortunately LT() doesn't use `get_ignore_mod_tap_interrupt`, so we'll
+  // have to emulate it's logic manually here
+  // https://github.com/qmk/qmk_firmware/issues/303
+  // https://github.com/yroeht/qmk_firmware/commit/cc78964f531fff7eb0ebc4c4e75bc8bc76386ec2
+  switch (keycode) {
+    case LT_LOWER:
+      if (record->event.pressed) {
+        lower_interrupted = false;
+        layer_on(_LOWER);
+      } else {
+        if (!lower_interrupted) {
+          register_code(KC_SPC);
+          unregister_code(KC_SPC);
+        }
+        layer_off(_LOWER);
+      }
+      return false;
+      break;
+    case LT_RAISE:
+      if (record->event.pressed) {
+        raise_interrupted = false;
+        layer_on(_RAISE);
+      } else {
+        if (!raise_interrupted) {
+          register_code(KC_SPC);
+          unregister_code(KC_SPC);
+        }
+        layer_off(_RAISE);
+      }
+      return false;
+      break;
+    default:
+      if (record->event.pressed) {
+        lower_interrupted = true;
+        raise_interrupted = true;
+      }
+  }
   return true;
 }
-#endif // OLED_DRIVER_ENABLE
+
+// some tap dance customization
 
 typedef enum {
   TD_NONE,
@@ -228,7 +312,8 @@ typedef enum {
 
 typedef struct {
   td_state_t state;
-  uint16_t tap;
+  uint16_t single_mod;
+  uint16_t single_tap;
   uint16_t double_mod;
   uint16_t double_tap;
   uint16_t hold;
@@ -252,10 +337,20 @@ void _tap_dance_hold_finished(qk_tap_dance_state_t *state, void *user_data) {
   td_tap_t* user_state = (td_tap_t*)user_data;
   user_state->state = cur_dance(state);
   switch (user_state->state) {
-    case TD_SINGLE_TAP: register_code(user_state->tap); break;
+    case TD_SINGLE_TAP:
+      if (user_state->single_mod) {
+        register_code(user_state->single_mod);
+      }
+      register_code(user_state->single_tap);
+      break;
     case TD_SINGLE_HOLD: register_code(user_state->hold); break;
     case TD_DOUBLE_TAP:
-      register_code(user_state->double_mod);
+      if (!user_state->double_tap) {
+        break;
+      }
+      if (user_state->double_mod) {
+        register_code(user_state->double_mod);
+      }
       register_code(user_state->double_tap);
       break;
     case TD_DOUBLE_HOLD: register_code(user_state->hold); break;
@@ -266,11 +361,21 @@ void _tap_dance_hold_finished(qk_tap_dance_state_t *state, void *user_data) {
 void _tap_dance_hold_reset(qk_tap_dance_state_t *state, void *user_data) {
   td_tap_t* user_state = (td_tap_t*)user_data;
   switch (user_state->state) {
-    case TD_SINGLE_TAP: unregister_code(user_state->tap); break;
+    case TD_SINGLE_TAP:
+      unregister_code(user_state->single_tap);
+      if (user_state->single_mod) {
+        unregister_code(user_state->single_mod);
+      }
+      break;
     case TD_SINGLE_HOLD: unregister_code(user_state->hold); break;
     case TD_DOUBLE_TAP:
+      if (!user_state->double_tap) {
+        break;
+      }
       unregister_code(user_state->double_tap);
-      unregister_code(user_state->double_mod);
+      if (user_state->double_mod) {
+        unregister_code(user_state->double_mod);
+      }
       break;
     case TD_DOUBLE_HOLD: unregister_code(user_state->hold); break;
     case TD_NONE: break;
@@ -282,10 +387,61 @@ qk_tap_dance_action_t tap_dance_actions[] = {
   [D_COLN] = {
     .fn = {NULL, _tap_dance_hold_finished, _tap_dance_hold_reset},
     .user_data = (void*)&((td_tap_t){
-      .tap = KC_SCLN,
+      .single_mod = KC_RSFT,
+      .single_tap = KC_SCLN,
+      .double_mod = 0,
+      .double_tap = 0,
+      .hold = KC_RSFT,
+    }),
+  },
+  [D_SCLN] = {
+    .fn = {NULL, _tap_dance_hold_finished, _tap_dance_hold_reset},
+    .user_data = (void*)&((td_tap_t){
+      .single_mod = 0,
+      .single_tap = KC_SCLN,
       .double_mod = KC_RSFT,
       .double_tap = KC_SCLN,
       .hold = KC_RSFT,
+    }),
+  },
+  [D_CIRC] = {
+    .fn = {NULL, _tap_dance_hold_finished, _tap_dance_hold_reset},
+    .user_data = (void*)&((td_tap_t){
+      .single_mod = KC_LSFT,
+      .single_tap = KC_6,
+      .double_mod = 0,
+      .double_tap = 0,
+      .hold = KC_LCTL
+    }),
+  },
+  [D_PERC] = {
+    .fn = {NULL, _tap_dance_hold_finished, _tap_dance_hold_reset},
+    .user_data = (void*)&((td_tap_t){
+      .single_mod = KC_LSFT,
+      .single_tap = KC_5,
+      .double_mod = 0,
+      .double_tap = 0,
+      .hold = KC_LALT,
+    }),
+  },
+  [D_DLR] = {
+    .fn = {NULL, _tap_dance_hold_finished, _tap_dance_hold_reset},
+    .user_data = (void*)&((td_tap_t){
+      .single_mod = KC_LSFT,
+      .single_tap = KC_4,
+      .double_mod = 0,
+      .double_tap = 0,
+      .hold = KC_LGUI,
+    }),
+  },
+  [D_TILD] = {
+    .fn = {NULL, _tap_dance_hold_finished, _tap_dance_hold_reset},
+    .user_data = (void*)&((td_tap_t){
+      .single_mod = KC_LSFT,
+      .single_tap = KC_GRV,
+      .double_mod = 0,
+      .double_tap = 0,
+      .hold = KC_LSFT,
     }),
   },
 };
